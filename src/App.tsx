3b88fffdfa2755786228513de8b82ca7006375f6
IvanVlKuchumov <kuchumoviv@gmail.com>
2022-12-18 22:25:40 +0300

    Add components Accordion.tsx and Rating.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import {Rating} from "./components/Rating/Rating";
+import {Accordion} from "./components/Accordion/Accordion";
 
 function App() {
     return (
-        <div>
+        <div className='content'>
             This is APP component
+            <Rating/>
+            <Accordion/>
         </div>
     );
 }
---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Star} from "./Star";
+
+export const Rating = () => {
+    return (
+        <>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </>
+    )
+}
