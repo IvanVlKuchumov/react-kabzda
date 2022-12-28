c40a02635d27909bd7c28905a47101ddb737100d
IvanVlKuchumov <kuchumoviv@gmail.com>
2022-12-28 19:32:51 +0300

    Add callBack function

--- src/components/Rating/RatingButton.tsx
+++ src/components/Rating/RatingButton.tsx
@@ -1,33 +1,26 @@
 import React, {useState} from "react";
 
 
-
 export const RatingButton = () => {
 
-    let [bold, setBold] = useState(false)
+    let [value, setValue] = useState(0)
 
 
     return (
         <div>
-            <Star bold={bold}/><button onClick={()=>setBold(!bold)}>*</button>
-            <Star bold={bold}/><button onClick={()=>setBold(!bold)}>*</button>
-            <Star bold={bold}/><button onClick={()=>setBold(!bold)}>*</button>
-            <Star bold={bold}/><button onClick={()=>setBold(!bold)}>*</button>
-            <Star bold={bold}/><button onClick={()=>setBold(!bold)}>*</button>
+            <Star selected={value > 0} setValue={setValue} value={1}/>
+            <Star selected={value > 1} setValue={setValue} value={2}/>
+            <Star selected={value > 2} setValue={setValue} value={3}/>
+            <Star selected={value > 3} setValue={setValue} value={4}/>
+            <Star selected={value > 4} setValue={setValue} value={5}/>
         </div>
     )
 }
 type StarPropsType = {
-    bold: boolean
+    selected: boolean
+    value: 1 | 2 | 3 | 4 | 5
+    setValue: (value: 1 | 2 | 3 | 4 | 5) => void
 }
-const Star = (props:StarPropsType) => {
-    if (props.bold) {
-        return (
-            <b>Star</b>
-        )
-    } else {
-        return (
-            <>Star</>
-        )
-    }
+const Star = (props: StarPropsType) => {
+    return <span onClick={() => props.setValue(props.value)}>{props.selected ? <b>Star</b> : 'Star'} </span>
 }
