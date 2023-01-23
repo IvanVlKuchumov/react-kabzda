7e10c3b8b5300998a15ff06701873bd3ac41719a
IvanVlKuchumov <kuchumoviv@gmail.com>
2023-01-23 21:13:53 +0300

    Work with Input and Select

--- src/stories/UncontrolledInput.stories.tsx
+++ src/stories/UncontrolledInput.stories.tsx
@@ -29,5 +29,45 @@ export const GetValueOfUncontrolledByButtonPress = () => {
     )
 }
 
-
 export const ControlledInputWithFixedValue = () => <input value={'Fsdviafdbsl;k'}/>
+export const ControlledInput = () => {
+
+    const [inputValue, setInputValue] = useState('')
+
+    const onChangeHandler = (e:ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.currentTarget.value)
+    }
+
+    return (
+        <><input onChange={onChangeHandler} value={inputValue}/> The value is taken from the data: value = {inputValue}</>
+    )
+}
+
+export const ControlledCheckbox = () => {
+
+    const [checkboxValue, setCheckboxValue] = useState(false)
+
+    const onChangeHandler = (e:ChangeEvent<HTMLInputElement>) => {
+        setCheckboxValue(e.currentTarget.checked)
+    }
+
+    return (
+        <><input type={"checkbox"} onChange={onChangeHandler} checked={checkboxValue}/> The value is taken from the data</>
+    )
+}
+
+export const ControlledSelect = () => {
+
+    const [selectValue, setSelectValue] = useState<string>('4')
+
+    const onChangeHandler = (e:ChangeEvent<HTMLSelectElement>) => {
+        setSelectValue(e.currentTarget.value)
+    }
+
+    return <><select name={'select'} value={selectValue} onChange={onChangeHandler}>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+        <option value="4">Four</option>
+    </select>  {selectValue} value from select</>
+}
